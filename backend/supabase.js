f867cdc08f4c8144f90729b1ec99063c6a2ac795
TestingByIdk <149605205+TestingByIdk@users.noreply.github.com>
2026-06-29 17:41:23 -0400

    Rename supabase variable to supabaseClient

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,9 +1,10 @@
 const SUPABASE_URL = "https://agyfowvvcowkuyalcemh.supabase.co";
 const SUPABASE_KEY = "sb_publishable_7SonysYn2EJ3_VCaT32DHg_vMqM_bmK";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
 console.log("🐙 Connected to NeedThingsDone!");
+
